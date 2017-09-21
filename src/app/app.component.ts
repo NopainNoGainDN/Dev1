8d06998a2da9cbdc6fa404c65e61c661ce43d2f5
Niang <dame.niang@cgi.com>
2017-09-21 13:45:12 +0200

    adding changes Project AngularCli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,220 @@
-import { Component } from '@angular/core';
+import { 
+  Component 
+} from '@angular/core';  
+
+import { 
+  appService 
+} from './app.service';  
+
+@Component ({ 
+  selector: 'my-app', 
+  template: '<div>{{value}}</div>', 
+  providers: [appService] 
+}) 
+
+export class AppComponent { 
+  value: string = ""; 
+  constructor(private _appService: appService) { }  
+
+  ngOnInit(): void { 
+     this.value = this._appService.getApp(); 
+  } 
+}
+
+
+/*import { 
+  Component 
+} from '@angular/core'; 
+
+import { 
+  ChildComponent 
+} from './child.component'; 
+
+@Component ({ 
+  selector: 'my-app', 
+  template: '<child-app></child-app> ' 
+}) 
+
+export class AppComponent { }
+
+*/
+
+/*import { 
+  Component 
+} from '@angular/core';  
+
+@Component ({ 
+  selector: 'my-app', 
+  template: '<div> {{values}} </div> ' 
+}) 
+
+export class AppComponent { 
+  values = ''; 
+  ngOnInit() { 
+     this.values = "Hello"; 
+  } 
+}*/
+
+
+
+/*import {
+  Component
+} from '@angular/core';
+
+@Component ({
+  selector: 'my-app',
+  template: `<button (Click) = "onClickMe()"> Click Me </button> {{clickMessage}}`
+})
+
+export class AppComponent {
+  clickMessage ='Hello';
+  onClickMe() {
+     this.clickMessage = 'This Tutorial!';
+  }
+}*/
+
+/*import { 
+  Component 
+} from '@angular/core';  
+
+@Component ({ 
+  selector: 'my-app', 
+  template: '<p>Multiplier : {{2 | Multiplier: "100"}}</p>' 
+}) 
+export class AppComponent {  } */
+
+
+/*import { 
+  Component 
+} from '@angular/core';  
+
+@Component ({ 
+  selector: 'my-app', 
+  templateUrl: './app.component.html' 
+}) 
+
+export class AppComponent { 
+  newValue: number = 0.3;
+  //newValue: number = 30; 
+  //newValue: number = 123; 
+} */
+
+
+
+/*import { 
+  Component 
+} from '@angular/core';  
+
+@Component ({ 
+  selector: 'my-app', 
+  templateUrl: './app.component.html' 
+}) 
+
+export class AppComponent { 
+  newdate = new Date();
+}
+*/
+
+
+/*import { 
+  Component 
+} from '@angular/core';  
+
+@Component ({ 
+  selector: 'my-app', 
+  templateUrl: './app.component.html' 
+}) 
+
+export class AppComponent { 
+  Status: boolean = true; 
+  clicked(event) { 
+     this.Status = false; 
+  } 
+}*/
+
+
+/*import {
+  Component
+} from '@angular/core';
+
+@Component ({
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+  TutorialName: string = 'Angular JS2';
+  appList: string[] = ["Binding", "Display", "Services"];
+}*/
+
+
+
+
+/*import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'my-app',
+  templateUrl: '/app.component.html'
+})
+
+export class AppComponent {
+  appTitle: string = 'Welcome';
+  
+  appList: any[] = [{
+     "ID": "1",
+     "Name": "One",
+     "url": 'app/Images/One.jpg'
+  },
+  {
+     "ID": "2",
+     "Name": "Two",
+     "url": 'app/Images/Two.jpg'
+  }];
+}*/
+
+
+
+/*import { Component } from '@angular/core';
+import {PaginatePipe, PaginationService} from 'ng2-pagination';
+
+@Component ({
+   selector: 'my-app',
+   template: ` 
+   <ul>
+   <li *ngFor = "let item of collection | paginate: {
+      itemsPerPage: 5, currentPage: p }"> ... </li>
+   </ul>
+   <pagination-controls (pageChange) = "p = $event"></pagination-controls>`
+})
+export class AppComponent { }*/
+
+
+
+
+
+
+
+
+/*import { Component } from '@angular/core';
+import { appService } from './products.service';  
+
+@Component({ 
+  selector: 'my-app', 
+  template: '<div>{{value}}</div>', 
+  providers: [appService]  
+}) 
+
+export class AppComponent { 
+  value: string = ""; 
+  constructor(private _appService: appService) { } 
+  ngOnInit(): void { 
+     this.value = this._appService.getApp(); 
+  }
+}
+*/
+
+
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +223,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+}*/
+
+//import { Component } from '@angular/core';
+//import { IProduct } from './iProducts';
+//import { ProductService } from './products.service';
+//import { appService } from './app.service';
+//import { Http , Response } from '@angular/http';
+//import { Observable } from 'rxjs/Observable';
+//import 'rxjs/add/operator/map';
+/*
+@Component ({
+   selector: 'my-app',
+   template: '<product-form></product-form>',
+   // providers: [ProductService]
+})
+
+export   class   AppComponent  {
+   iproducts: IProduct[];
+   constructor(private _product: ProductService) {
+   }
+   
+   ngOnInit() : void {
+      this._product.getproducts()
+      .subscribe(iproducts => this.iproducts = iproducts);
+   }*/
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,48 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import { MultiplierPipe } from './multiplier.pipe'
+import {Ng2PaginationModule} from 'ng2-pagination';
+import {  ChildComponent } from './child.component';  
+
+
+
+@NgModule ({
+  imports:      [ BrowserModule,Ng2PaginationModule],
+  declarations: [ AppComponent, MultiplierPipe, ChildComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
+
+
+//import { HttpModule } from '@angular/http';
+//import { FormsModule } from '@angular/forms';
+//import { ProductFormComponent } from './product-form.component';
+
+
+/*import { Appproduct } from './product.component';
+import { AppInventory } from './Inventory.component';
+import { PageNotFoundComponent } from  './NotFound.component'
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'Product', component: Appproduct },
+  { path: 'Inventory', component: AppInventory },
+  { path: '**', component: PageNotFoundComponent }
+];*/
+
+/*@NgModule ({
+  //imports: [ BrowserModule,RouterModule.forRoot(appRoutes)],
+  //declarations: [ AppComponent,Appproduct,AppInventory,PageNotFoundComponent], 
+  imports: [ BrowserModule,FormsModule],
+  declarations: [ AppComponent,ProductFormComponent],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }*/
+
+
+
+/*import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -14,3 +58,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+*/
